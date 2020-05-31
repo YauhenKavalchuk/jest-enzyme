369cca8780bca27b756921d5dd562493d045aa14
Yauhen <silent_control@tut.by>
2020-05-31 11:46:07 +0300

    Add toJson to global scope

--- config/setupTest.js
+++ config/setupTest.js
@@ -1,5 +1,6 @@
 import Enzyme, { shallow, render, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import toJson from 'enzyme-to-json';
 
 // React 16 Enzyme adapter
 Enzyme.configure({ adapter: new Adapter() });
@@ -7,6 +8,7 @@ Enzyme.configure({ adapter: new Adapter() });
 global.shallow = shallow;
 global.render = render;
 global.mount = mount;
+global.toJson = toJson;
 
 // Fail tests on any warning
 console.error = message => {

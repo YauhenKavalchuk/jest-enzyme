935527f4df2fce58871f22b1630c600f8250d76e
Yauhen <silent_control@tut.by>
2020-06-07 11:30:23 +0300

    Update logic for Posts component. Add API constants

--- src/Posts/constants.js
+++ src/Posts/constants.js
@@ -37,3 +37,9 @@ export const HITS = [
     label: 50,
   },
 ];
+
+export const BASE_PATH = 'https://hn.algolia.com/api/v1';
+export const SEARCH_PATH = '/search';
+export const SEARCH_PARAM = 'query=';
+export const PAGE_HITS = 'hitsPerPage=';
+export const PAGE_PARAM = 'page=';
--- src/Posts/posts.js
+++ src/Posts/posts.js
@@ -4,14 +4,40 @@ import Post from "../Post/post";
 import Title from "../Title/title";
 import Select from "../Select/select";
 import Input from "../Input/input";
+import Pagination from "../Pagination/pagination";
 
-import { NEWS, HITS } from "./constants";
+import "./posts.css";
+
+import {
+  NEWS,
+  HITS,
+  BASE_PATH,
+  SEARCH_PATH,
+  SEARCH_PARAM,
+  PAGE_HITS,
+  PAGE_PARAM,
+} from "./constants";
 
 class Posts extends Component {
   state = {
     searchQuery: "",
+    result: {},
     hitsPerPage: 20,
     page: 0,
+  };
+
+  componentDidMount() {
+    const { searchQuery, hitsPerPage, page } = this.state;
+    this.fetchData(searchQuery, hitsPerPage, page);
+  }
+
+  fetchData = (searchQuery, hitsPerPage, page) => {
+    fetch(
+      `${BASE_PATH}${SEARCH_PATH}?${SEARCH_PARAM}${searchQuery}&${PAGE_HITS}${hitsPerPage}&${PAGE_PARAM}${page}`
+    )
+      .then((res) => res.json())
+      .then((result) => this.setNews(result))
+      .catch((error) => error);
   };
 
   handleInputChange = ({ target: { value } }) => {
@@ -21,22 +47,64 @@ class Posts extends Component {
   };
 
   handleHitsChange = ({ target: { value } }) => {
-    this.setState({
-      hitsPerPage: +value,
-      page: 0,
-    });
+    const { searchQuery } = this.state;
+
+    this.setState(
+      {
+        hitsPerPage: +value,
+        page: 0,
+      },
+      () => {
+        this.fetchData(searchQuery, this.state.hitsPerPage, 0);
+      }
+    );
   };
 
   getSearch = ({ key }) => {
     if (key === "Enter") {
+      const { searchQuery, hitsPerPage } = this.state;
       this.setState({
         page: 0,
       });
+      this.fetchData(searchQuery, hitsPerPage, 0);
     }
   };
 
-  render() {
+  setNews = (result) => {
+    this.setState({ result });
+  };
+
+  handlePageChange = ({ target }) => {
+    const btnType = target.getAttribute("data-name");
+    let { page } = this.state;
+
+    if (!isNaN(btnType)) {
+      this.updatePage(+btnType);
+    } else {
+      if (btnType === "next") {
+        this.updatePage(page + 1);
+      }
+      if (btnType === "prev") {
+        this.updatePage(page - 1);
+      }
+    }
+  };
+
+  updatePage = (number) => {
     const { searchQuery, hitsPerPage } = this.state;
+    this.setState(
+      {
+        page: number,
+      },
+      () => {
+        this.fetchData(searchQuery, hitsPerPage, number);
+      }
+    );
+  };
+
+  render() {
+    const { searchQuery, result, hitsPerPage } = this.state;
+    const { hits = [], page, nbPages } = result;
 
     return (
       <div className="wrapper">
@@ -46,13 +114,20 @@ class Posts extends Component {
           handleChange={this.handleHitsChange}
           value={hitsPerPage}
         />
+        {hits.length > 0 && (
+          <Pagination
+            onClick={this.handlePageChange}
+            page={page}
+            lastPage={nbPages}
+          />
+        )}
         <Input
           onKeyPress={this.getSearch}
           onChange={this.handleInputChange}
           value={searchQuery}
         />
         <ul className="newsList">
-          {NEWS.map(
+          {hits.map(
             ({
               author,
               created_at,
